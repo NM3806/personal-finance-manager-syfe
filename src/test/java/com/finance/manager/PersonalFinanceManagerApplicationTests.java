6e37aedbaf7252bf45fe5f0d8895ce3ad5c84294
Naitik Madarwal <naitikmadarwal3806@gmail.com>
2026-09-04 11:48:22 +0530

    chore: initialize Spring Boot project

---
+++ src/test/java/com/finance/manager/PersonalFinanceManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.finance.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
